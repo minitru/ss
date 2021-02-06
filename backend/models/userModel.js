46cb04703457a9028e50cba1766c89e9b81f563b
Sean MacGuire <sean@maclawran.ca>
2021-02-06 03:42:57 +0000

    add googleid

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
+  googleid: { type: String, required: true },
   name: { type: String, required: true },
   email: {
     type: String, required: true, unique: true, index: true, dropDups: true,
   },
   password: { type: String, required: true },
-  googleid: { type: String, required: false },
   isAdmin: { type: Boolean, required: true, default: false },
 });
 
